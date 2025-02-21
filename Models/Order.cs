378b53d127672af8a30d82255f07b65dc0f2f013
Aaron Colaco <ac121654@avcol.school.nz>
2025-02-21 13:34:09 +1300

    Over the past week I worked on my design of the front end of my website. Today I worked on the bridge mode. *Added relationship between item and order(many to many)
    To find out why I chose to use these relationships please refer to my design in my documentation. I have provided a significant amount of detail about the design processes I followed and an clear explaintion of how my relationship between my models is setup.
    
     A 'bridge' model/entity for the 'order item' relationship to facilitate the many-to-many relationship between items and orders. This is necessary because multiple items can be associated with one order, and also, one item can be part of multiple orders.

--- Models/Order.cs
+++ Models/Order.cs
@@ -15,11 +15,13 @@ namespace WebApplication4.Models
         [Required]
         public DateTime OrderTime { get; set; } = DateTime.Now;
 
+        public DateTime HireEndDate { get; set; }; 
+
         // Total price of the order, required to be between 1 and 1000, formatted as currency
         [DataType(DataType.Currency), Range(1, 1000)]
         public decimal TotalPrice { get; set; }
 
-        public DateTime HireEndDate{get; set;} 
+   
 
 
 
---
+++ Models/OrderItem.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication4.Models
+{
+    public class OrderItem
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+
+        //Primary key feild
+        public string OrderItemId { get; set; }
+
+        //sets the defualt quantity to 1
+        public int Quantity { get; set; } = 1;
+
+        //foreign key to set up the relastonship with the order table(many to many)
+        [ForeignKey("Order")]
+        public string OrderId { get; set; }
+        public Order Orders { get; set; }
+
+        //foreign key to set up the relastonship with the item table(many to many)
+        [ForeignKey("Item")]
+        public int ItemId { get; set; }
+        public Item Items { get; set; }
+
+      
+
+
+
+    }
+}
