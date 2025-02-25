0785bc6d0339b8f7f0576edb6e43c611317f98a9
Aaron-Colaco <ac121654@avcol.school.nz>
2025-02-26 10:10:00 +1300

    Add packasges for identity

---
+++ Models/User.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.General;
+
+namespace WebApplication4.Models
+{
+    public class User : IdentityUser
+    {
+        public string FirstName { get; set; }
+        
+       public string LastName { get; set; }
+
+        public string DOB { get; set; }
+
+        public string StudentNumber { get; set; }
+
+        public int year { get; set; }
+
+    }
+}
