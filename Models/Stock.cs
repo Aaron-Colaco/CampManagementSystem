1a90a57688b660759c7102b68474bf4cafe8990f
Aaron-Colaco <ac121654@avcol.school.nz>
2025-04-04 16:40:47 +1300

    changed register to have a more user friendly way to pick year level. (drop-down list)

--- Models/Stock.cs
+++ Models/Stock.cs
@@ -6,13 +6,15 @@ namespace WebApplication4.Models
     public class Stock
     {
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+
         public int StockId { get; set; }
 
          [ForeignKey("Item")]
         public int ItemId { get; set; }
 
         [ForeignKey("User")]
-        public string UserId { get; set; }
+        public string? UserId { get; set; }
 
         public User user { get; set; }
 
